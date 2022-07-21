b3dae50c8719559cfad38f98c382bc5a4aa1d104
Shanghai.2020 <glen.bowler2019@gmail.com>
2022-07-21 14:27:41 +0200

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -37,6 +37,9 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
+            //Add cors
+            services.AddCors();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,10 +52,19 @@ namespace API
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
             }
             //Middleware
-            app.UseHttpsRedirection();
+           
 
             app.UseRouting();
 
+            //Setting up cors
+            app.UseCors(policy=>policy
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            // .WithOrigins("http://localhost:4200"));
+            .AllowAnyOrigin());
+
+             app.UseHttpsRedirection();
+             
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
